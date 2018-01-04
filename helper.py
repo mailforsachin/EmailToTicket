eccd5ce2ae50eba5a65cf11e796949dfd355b4cd
Setia <tarik@byondreal.net>
2018-01-04 16:25:58 +0530

    intent

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 
 import spacy
 import numpy as np
+import requests
 def getContact(mailbox):
 	return {'name':mailbox.name, 'email':mailbox.email_address}
 
@@ -40,30 +41,33 @@ def process(text,pos=pos):
 	message = []
 	temp = []
 	disclaimer = False
+	from_email = ''
 	while True:
 		try:
 			line = text.pop().encode('ascii',errors = 'ignore')
 			line = line.strip()
 			nospace = line.replace(' ','')
-			if line =='' or '-----' in line or '_____' in line:
+			if line =='' or '-----' in line :
 				continue
 			if len(line.split()) == 1 and len(line)>10:
 				continue
 			if line[:6] == 'From: ':
+				from_email = line[6:]
 				disclaimer = False
+				#msg_str = ' '.join(temp)
 				message.append(temp)
 				temp=[]
 				#print line
 			elif line[:4] == 'To: ' or line[:4] == 'Cc: ' or line[:9] == 'Subject: ' or line[:4] == 'Cc: ' or  line[:6] == 'Sent: ':
 				#print line
 				continue
-			elif line.lower().find('disclaimer') == 0:
+			elif line.lower().find('disclaimer') == 0 or '__________' in line or line[0:].lower() in from_email.lower():
 				disclaimer = True
 				#print line
 				continue
 			elif line.lower().find('[cid:image') ==0:
 				continue
-			elif ('classification' in line.lower() or 'hello' in line.lower() or 'thank' in line.lower() or 'regard' in line.lower() or line[:4].lower() == 'best' or 'hi' in line.lower()) and prob_block(line,pos) >=0.5:
+			elif ('classification' in line.lower() or 'hello' in line.lower() or 'thank' in line.lower() or 'regard' in line.lower() or line[:4].lower() == 'best' or 'hi' in line.lower()) and prob_block(line,pos) >=0.9:
 				continue
 			elif (len(nospace) < 50 or '|' in line ) and prob_block(line,pos) >=0.9:
 				continue
@@ -79,11 +83,45 @@ def process(text,pos=pos):
 		except Exception,e:
 			print str(e)
 			pass
+	#msg_str = ' '.join(temp)
 	message.append(temp)
 	disclaimer = False
 	return (message)
 
 
+def getLuisIntent(utterance):
+	luis_url = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/50e40c1c-efa2-48dd-bca3-d868ef5ed4c4?subscription-key=62ea92a6dad241628c1489c0333d166f&verbose=true&timezoneOffset=0&q=" + utterance[:500]
+	res = requests.get(luis_url)
+	if res.status_code == 200:
+		vals = res.json()
+		score = vals['topScoringIntent']['score']
+		intent = vals['topScoringIntent']['intent']
+		return (str(intent),float(score))
+
+def getIntent(msgs):
+	intents ={}
+	for msg in msgs:
+		utt = ' '.join(msg).strip()
+		intent, score = getLuisIntent(utt)
+		if score>0.9:
+			return {'intent':intent,'utterance':utt}
+			break
+
+	return {'intent':None, 'utterance':None}
+
+def getIntentPerLine(msgs):
+	intent_dic = {}
+	for msg in msgs:
+		for line in msg:
+			print line
+			if prob_block(line) >=.6:
+				intent, score = getLuisIntent(line)
+				if score>0.85:
+					if intent_dic.get(intent):
+						intent_dic[intent].append(line)
+					else:
+						intent_dic[intent] = [line]
+	return intent_dic
 
 
 
--- reader.py
+++ reader.py
@@ -15,14 +15,14 @@ from hashlib import md5
 print "Loading Spacy"
 pos = spacy.load('en')
 print "spacey loaded"
-credentials = Credentials(username='127334@nttdata.com', password='$')
+credentials = Credentials(username='127334@nttdata.com', password='')
 
 print "connecting to exchange"
 account = Account(primary_smtp_address='127334@nttdata.com', credentials=credentials,autodiscover=True, access_type=DELEGATE)
 print "connected"
 
 
-unread  = account.inbox.filter(is_read=True).only('sender','to_recipients','cc_recipients','subject','datetime_received','text_body','body').order_by('-datetime_received')[:1]
+unread  = account.inbox.filter(is_read=False).only('sender','to_recipients','cc_recipients','subject','datetime_received','text_body','body').order_by('-datetime_received')[:3]
 #unread  = account.inbox.filter(is_read=False).order_by('-datetime_received')[:1]
 
 emails = [i for i in unread]
@@ -33,62 +33,46 @@ luis_url = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/b922584b-1
 
 #MongoDB Connection
 client = pymongo.MongoClient('mongodb://serivice-desk-store:pMzmGDeCcbMZEJ7hVirY9YEA7NMm6abpGF6oNz32D6MNTqsmdqzvdg3MLVw80eMlztxPP03vxsKxtatgPOiCFg==@serivice-desk-store.documents.azure.com:10255/?ssl=true&replicaSet=globaldb')
+#client = pymongo.MongoClient('localhost', 27017)
 db = client.hilton
 sdmails  = db.emails
 
 #Azure Service Bus Connection
+nttBus = ServiceBusService(
+    service_namespace='ntt-bus',
+    shared_access_key_name='RootManageSharedAccessKey',
+    shared_access_key_value='ak9L18tmI2FssJBIZLz3OCs8U55rcYZaSbwgAR6/B34=')
+
+
 
 for email in emails:
 	try:
-		mail = {}
-		mail['from'] = fx.getContact(email.sender)
-		mail['to'] = fx.getContacts(email.to_recipients)
+		# Split and Parse emails
+		# Get Contents
+		# Save to db
+		doc = {}
+		doc['from'] = fx.getContact(email.sender)
+		doc['to'] = fx.getContacts(email.to_recipients)
 		if email.cc_recipients:
-			mail['cc'] = fx.getContacts(email.cc_recipients) 
-		mail['subject'] = email.subject
+			doc['cc'] = fx.getContacts(email.cc_recipients) 
+		doc['subject'] = email.subject.strip()
 		msgs = fx.process(email.text_body)
-		uniqueMessage = msgs[0]
-		mailtrail = msgs[1:]
-		mail['message'] = uniqueMessage
-		jmsg = [' '.join for i in msgs]
-
-		allemails = []
-		for utt in jmsg:
-			url = luis_url + str(utt)
-			res = requests.get(url)
-			if res.status_code == 200:
-				vals = res.json()
-				score = vals['topScoringIntent']['score']
-				intent = vals['topScoringIntent']['intent']
-				m  = {'intent':intent, 'message':utt}
-				allemails.append(m)
-
-		mail[emails] = allemails
-
-		print allemails
-
-		utt = ''.join(uniqueMessage)
-		url = luis_url + str(utt)
-		res = requests.get(url)
-		if res.status_code == 200:
-			vals = res.json()
-			mail['intent'] = vals
-
-		id = sdmails.insert_one(mail).inserted_id
+		msgs = [[doc['subject']]] + msgs
+		doc['emails'] = msgs
+		doc['intent'] = fx.getIntent(msgs)
+		#doc['intentions'] = fx.getIntentPerLine(msgs)
+		doc['caseid'] = str(uuid.uuid4())
+		doc['state'] = 'new'
+		doc['handoff'] = False
+		doc['botHasReplied'] = False
+		#print docid
+		
+		#Send Message to Incoming Queue
+		event = Message(json.dumps(doc))
+		nttBus.send_queue_message('htn.incoming.emails', event)
+		docid = sdmails.insert_one(doc).inserted_id
 	except Exception,e:
 		print email.subject
 		print str(e)
 		pass
 
-'''
-for line in uniqueMessage:
-			url = luis_url + str(line)
-			res = requests.get(url)
-			if res.status_code == 200:
-				vals = res.json()
-				score = vals['topScoringIntent']['score']
-				intent = vals['topScoringIntent']['intent']
-				if float(score)>=0.9:
-					mail['intent'] = vals
-
-'''
